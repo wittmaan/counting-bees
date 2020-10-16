226149b11390477e661fd8d404f240eb56c82950
wittmaan <wittmaan@gmx.de>
2020-10-16 21:04:59 +0200

    added bash script to run detection

---
+++ src/main/bash/04_detect.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python ../python/detect.py --input_file="../../../data/raw/video/Video20200827165551.mp4" --output_file="../../../data/detections/Video20200827165551.mp4" --weights_path="../../../data/training/model_checkpoint.pt"
